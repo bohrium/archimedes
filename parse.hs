14fa5a14f31f63564a13c96e2c9234b4f2ce1a36
Samuel Tenka <samtenka@umich.edu>
2020-09-30 03:31:21 -0400

    differentiation

--- parse.hs
+++ parse.hs
@@ -134,7 +134,7 @@ showtree et =
                Num q         -> show q
                Var nm        -> nm
                Theta n e     -> (theta_derivative_name n) ++ "("++(showtree e)++")" 
-               Smooth nms nm -> if nms==[] then nm  
+               Smooth nms nm -> if null nms then nm  
                                 else            (lotsa_ds nms) ++ "["++nm++"]"
 
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -184,13 +184,37 @@ smooth = do c <- sats (\c -> elem c "g")
 -- ===  3. TRANSLATE  =========================================================
 -- ============================================================================
        
+diff :: String -> ETree -> ETree   
+diff wrt et = case et of Sum es        -> Sum (map (diff wrt) es)
+                         Product es    -> let rr = [0 .. (length es - 1)] in
+                                              Sum (map (\i -> Product (map (\j -> if i==j then (diff wrt (es !! j)) else (es !! j)) rr)) rr)
+                         Num q         -> Num 0
+                         Var nm        -> if nm == wrt then Num 1 else Num 0 
+                         Theta n e     -> Product [Theta (n+1) e, diff wrt e]
+                         Smooth nms nm -> Smooth (wrt:nms) nm
+
+simp :: ETree -> ETree
+simp et = case et of Sum es        -> let terms = map simp es
+                                          nonzero = filter (\c -> case c of Num 0.0 -> False
+                                                                            _ -> True) terms in
+                                          if null nonzero then Num 0.0 else Sum nonzero
+                     Product es    -> let facts = map simp es
+                                          haszero = or (map (\c -> case c of Num 0.0 -> True
+                                                                             _ -> False) facts) 
+                                          nonone = filter (\c -> case c of Num 1.0 -> False
+                                                                           _ -> True) facts in
+                                          if haszero then Num 0.0 else
+                                              if null nonone then Num 1.0 else Product nonone
+                     Theta n e     -> Theta n (simp e) 
+                     _             -> et
+
 -- ============================================================================
 -- ===  4. MAIN LOOP  =========================================================
 -- ============================================================================
 
 try_parse s = case parse expr s of
                    Nothing -> "Failed to Parse" 
-                   Just (a, out) -> showtree a
+                   Just (a, out) -> showtree (simp (diff "x" a))
 
 main = do putStr "hello!\n\n"
           contents <- readFile "moo.txt" 
